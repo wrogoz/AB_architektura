3029c125a0ce0f1510fd2e5803755ebaea43db46
Wojciech Rogóż <wrogoz@gmail.com>
2019-04-04 13:50:13 +0200

    entries - in progress / added masks, icons, styling css

--- src/components/entries/entries.jsx
+++ src/components/entries/entries.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol,MDBIcon } from "mdbreact";
 import './entries.css'
 import Image from './image/image'
 import img1 from './images/10.jpg';
 import img2 from './images/11.jpg';
 import img3 from './images/17.jpg';
 import img4 from './images/26.jpg';
+import img5 from './images/29.jpg';
+import img6 from './images/E.jpg';
+import img7 from './images/15.jpg';
+import img8 from './images/20.jpg';
 
-
-const sources=[img1,img2,img3,img4];
 
 const Entries = props => {
     return (
@@ -18,16 +20,38 @@ const Entries = props => {
                     <Image imgSrc={img1}/>
                 </MDBCol>
                 <MDBCol sm="12" md="6">
-                <Image imgSrc={img2}/>
+                    <Image imgSrc={img2}/>
                 </MDBCol>
             </MDBRow>
 
             <MDBRow >
                 <MDBCol sm="12" md="6">
-                <Image imgSrc={img3}/>
+                    <Image imgSrc={img3}/>
                 </MDBCol>
                 <MDBCol sm="12" md="6">
-                <Image imgSrc={img4}/>
+                    <Image imgSrc={img4}/>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow >
+                <MDBCol sm="12" md="6">
+                    <Image imgSrc={img5}/>
+                </MDBCol>
+                <MDBCol sm="12" md="6">
+                    <Image imgSrc={img6}/>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow >
+                <MDBCol sm="12" md="6">
+                    <Image imgSrc={img7}/>
+                </MDBCol>
+                <MDBCol sm="12" md="6">
+                    <Image imgSrc={img8}/>
+                </MDBCol>
+            </MDBRow>
+            <MDBRow className="iconRow ">
+                <MDBCol className="iconsCol entries_icon" sm="12">
+                    <MDBIcon fab icon="facebook-f" />
+                    <MDBIcon fab icon="instagram"  />
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
--- src/components/entries/image/image.jsx
+++ src/components/entries/image/image.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './image.css'
-
+import { MDBMask, MDBView } from "mdbreact";
 const Image = props =>{
 
     let style={
         backgroundImage: `url(${props.imgSrc})`
     }
-    return <div className="imageSet" style={style}>
-        
-    </div>
+    return (
+        <MDBView hover>
+        <div className="imageSet" style={style}>
+        <MDBMask className="flex-center" overlay="stylish-strong">
+                <p className="white-text mask__txt">A + B Architektura</p>
+              </MDBMask>
+              </div>
+        </MDBView>
+
+    )
 }
 
 export default Image;
